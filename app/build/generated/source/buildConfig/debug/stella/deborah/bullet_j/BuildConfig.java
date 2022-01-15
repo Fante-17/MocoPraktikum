6b08d997e5e95eb5c27d551cbe8d53cd8f6b4c6e
Ornela  Fante <64073462+Fante-17@users.noreply.github.com>
2022-01-15 22:23:57 +0100

    first page created

---
+++ app/build/generated/source/buildConfig/debug/stella/deborah/bullet_j/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package stella.deborah.bullet_j;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "stella.deborah.bullet_j";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
